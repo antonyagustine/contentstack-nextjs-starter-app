f4eb3534083d874dd872f4211faf311ccb115a60
Antony <antonyagustineraj@gmail.com>
2023-03-14 11:25:27 +0530

    Server/lamda functions added

---
+++ functions/user.js
@@ -0,0 +1,6 @@
+export default function handler(req, res) {
+  if (req.method === "GET") {
+    console.log("processing user........");
+    res.status(200).json({ method: "GET", route: "id", params: req.params });
+  }
+}
